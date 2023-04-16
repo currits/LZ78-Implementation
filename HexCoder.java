544a5371b2f6174acf24d523be4190f3e212eec0
Mokaraka, Kurtis <khm7@students.waikato.ac.nz>
2023-04-17 09:19:35 +1200

    Reworked loop logic so prevenet printing -1 to output when encoding, and to handle hex encoded inputs that are of odd length. Tested on simple text file, currently works. Added some comments

--- HexCoder.java
+++ HexCoder.java
@@ -1,35 +1,38 @@
 import java.io.*;
 public class HexCoder {
-    static int mask = 0xF0;
+    static int mask = 0x0F;
+    static String delimiter = " ";
     public static void main(String[] args){
-        if (!(args.length == 0)) {
+        // We must decide if we want our byte-to-hex ouput to be raw hex data or written as strings mapping to hex ie 1-F
+        // current writes raw hex values seperated by spaces, change the delimiter as needed     
+        // and proccesses input as if there is a character (a space) in between each wanted character
+        if (args.length == 0) {
             System.out.println("Usage: HexCoder {0|1}");
             System.out.println("0:  Encodes Standard input stream into Hexadecimal");
             System.out.println("1:  Decodes Standard input stream from Hexadecimal into bytes");
             return;
         }
-        //when back check for if & operation gives 1011 or 00001011 and if that matters
-        //is hex hex if its more than four bits?
-        //is all this right?
-        
         try {
             int param = Integer.parseInt(args[0]);
+            //if encoding to hex
             if (param == 0){
                 try {
                     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
                     BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
                     int hexIn = 0;
-                    int nibble;
-                    //implement check for when read returns <8
-                    while (hexIn != -1){
+                    int nibbleUpper;
+                    int nibbleLower;
+                    while (true){
                         hexIn = reader.read();
-                        nibble = hexIn & mask;
-                        writer.write(nibble);
-                        writer.newLine();
-                        hexIn = hexIn << 4;
-                        nibble = hexIn & mask;
-                        writer.write(nibble);
-                        writer.newLine();
+                        if (hexIn == -1)
+                            break;
+                        nibbleLower = hexIn & mask;
+                        hexIn = hexIn >> 4;
+                        nibbleUpper = hexIn & mask;
+                        writer.write(nibbleUpper);
+                        writer.write(delimiter);
+                        writer.write(nibbleLower);
+                        writer.write(delimiter);
                     }
                     writer.flush();
                     writer.close();
@@ -40,21 +43,32 @@ public class HexCoder {
                     e.printStackTrace();
                 }
             }
+            //if decoding to ascii
             else if (param == 1){
                 try {
                     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
                     BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-                    int byteIn = 0;
                     byte byteOut;
-                    int hexIn1;
-                    int hexIn2;
-                    //implement check for when read returns <8
-                    while (byteIn != -1){
-                        byteIn = reader.read();
-                        hexIn1 = byteIn & mask;
-                        byteIn = byteIn << 4;
-                        hexIn2 = byteIn & mask;
-                        byteOut = (byte) ((hexIn1 << 4) | hexIn2);
+                    int hexIn1 = 0;
+                    int hexIn2 = 0;
+                    //need to implement handling of loop condition true when cleanly reaching end of stream
+                    //need to implement handling of hexIn1 != -1 and hexIn2 == -1
+                    while (true){
+                        //reads assuming a space character seperating every hex value
+                        hexIn1 = reader.read();
+                        //skip the space after each read character
+                        reader.read();
+                        hexIn2 = reader.read();
+                        reader.read();
+                        if (hexIn2 != -1){
+                            byteOut = (byte) ((hexIn1 << 4) | hexIn2);
+                        }
+                        else if (hexIn1 != -1){
+                            byteOut = (byte)hexIn1;
+                        }
+                        else{
+                            break;
+                        }
                         writer.write(byteOut);
                     }
                     writer.flush();
