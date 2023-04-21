e21bc2f35bdc1b6f6971f257e263a14adea05a3d
Espi <ethyn.gillies@gmail.com>
2023-04-21 18:44:15 +1200

    Hexcoder adjustment
    hexcoder still kinda broken, files with normal text is fine

--- HexCoder.java
+++ HexCoder.java
@@ -30,6 +30,8 @@ public class HexCoder {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+
         // if encoding to hex
         if (param == 0) {
             int c;
@@ -42,24 +44,58 @@ public class HexCoder {
         }
         // if decoding to ascii
         else if (param == 1) {
-            int c1;
-            int c2;
 
-            // We can read a second character inside the while loop since it will always be
-            // non-null due to eacha ascii character needing 2 hex digits
-            while ((c1 = reader.read()) != -1) {
-                c2 = reader.read();
+            StringBuilder sb = new StringBuilder();
 
-                // Create a string with both characters (we trim it because read() works weird
-                // sometimes
-                String str = ("" + (char) c1 + (char) c2).trim();
+            String hex = reader.readLine();
+            System.out.println(hex);
 
-                // Check if the string is empty, if not, parse it as a character using radix 16
-                // (base16 aka hexadecimal)
-                if (!str.equals("")) {
-                    writer.write((char) Integer.parseInt(str, 16));
-                }
+            for (int i = 0; i < hex.length() - 1; i += 2) {
+
+                // grab the hex in pairs
+                String output = hex.substring(i, (i + 2));
+                // convert hex to decimal
+                int decimal = Integer.parseInt(output, 16);
+                // convert the decimal to character
+                sb.append((char) decimal);
             }
+
+            writer.write(sb.toString());
+
+            /*
+             * int c1 = 0;
+             * int c2 = 0;
+             * 
+             * // We can read a second character inside the while loop since it will always
+             * be
+             * // non-null due to eacha ascii character needing 2 hex digits
+             * try {
+             * while ((c1 = reader.read()) != -1) {
+             * 
+             * if ((c2 = reader.read()) == -1) {
+             * continue;
+             * }
+             * 
+             * // Create a string with both characters (we trim it because read() works
+             * weird
+             * // sometimes
+             * String str = ("" + (char) c1 + (char) c2).trim();
+             * 
+             * // Check if the string is empty, if not, parse it as a character using radix
+             * 16
+             * // (base16 aka hexadecimal)
+             * if (!str.equals("")) {
+             * writer.write((char) Integer.parseInt(str, 16));
+             * }
+             * }
+             * } catch (Exception e) {
+             * System.out.println(c1);
+             * System.out.println(c2);
+             * e.printStackTrace();
+             * return;
+             * }
+             */
+
         } else {
             System.out.println("Usage: HexCoder {0|1}");
             System.out.println("0:  Encodes Standard input stream into Hexadecimal");
